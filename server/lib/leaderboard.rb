a19b90dbdc448d51836072c23f380cb8487659ae
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-06-28 19:28:29 +0100

    Fix leaderboard time query bug

--- server/lib/leaderboard.rb
+++ server/lib/leaderboard.rb
@@ -1,10 +1,10 @@
 require_relative './flight'
 
 class Leaderboard
-  ONE_DAY = 60 * 60
+  ONE_HOUR = 60 * 60
 
   def self.best_today(limit: 100)
-    day_ago = Time.now - ONE_DAY
+    day_ago = Time.now - 24 * ONE_HOUR
     Flight
       .where('submitted_at > ?', day_ago)
       .order(:max_altitude)
@@ -14,7 +14,7 @@ class Leaderboard
   end
 
   def self.best_this_week(limit: 100)
-    week_ago = Time.now - 7 * 24 * ONE_DAY
+    week_ago = Time.now - 7 * 24 * ONE_HOUR
     Flight
       .where('submitted_at > ?', week_ago)
       .order(:max_altitude)
@@ -24,7 +24,7 @@ class Leaderboard
   end
 
   def self.best_this_month(limit: 100)
-    month_ago = Time.now - 30 * 24 * ONE_DAY
+    month_ago = Time.now - 30 * 24 * ONE_HOUR
     Flight
       .where('submitted_at > ?', month_ago)
       .order(:max_altitude)
